579cbb15abd0733a57ad241c6fbd1ea4172b3ca3
fan <291950304@qq.com>
2023-03-10 11:36:36 +0800

    chore: layout

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,38 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import './style.scss'
+import App from './App'
+import { MyRouteType } from './mytypes'
 
-createApp(App).mount('#app')
+// 找到 examples 下的文件
+const examples = import.meta.glob('./examples/**/*.tsx')
+console.log(examples);
+
+const routes: MyRouteType[] = []
+
+const examplePromises = Object.keys(examples).map(x => examples[x]).map(f => f())
+
+Promise.all(examplePromises).then(list=> {
+
+  for (let module of list) {
+    
+    if (typeof module === 'object') {
+      for (let key in module) {
+        routes.push({
+          path : "/" + key.toLocaleLowerCase(),
+          key,
+          component : (module as any)[key]
+        })
+      }
+    }
+  }
+
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  })
+  
+  createApp(App, { routes }).use(router).mount('#app')
+})
+
+
